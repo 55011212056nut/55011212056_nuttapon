53b51a9e2b2e5866c8b5019403b2bc572b74ea9d
55011212056nut <istudents@iClassrooms-iMac-14.local>
2015-01-16 16:57:20 +0700

    week1
    basic

---
+++ test.playground/section-1.swift
@@ -0,0 +1,89 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let x=5
+let y=10
+var aa = x+y
+
+
+let p:Int = -19.99
+let ggg:Double = -19.99
+
+var shop = [String]()
+ shop = ["a","bfff"]
+
+println("list \(shop.count) items.")//นับจำนวน arr
+
+shop.append("Flour") //เพิ่ม  flour ใน arr
+
+println(shop)
+
+shop[0]="sixx" // เพิ่ม six index 0
+
+println(shop)
+
+//shop[0,1] = ["one","two"]
+
+println(shop)
+
+//------------------------------------boolean
+
+let onsale = true
+let notsale = true
+
+
+if (onsale){
+
+    println("yes")
+}
+else {
+    println("no")
+}
+
+if (notsale){
+    println("yes not sale")
+}
+else {
+
+}
+
+
+
+//*******************************
+
+let arrint:[Double]=[0.15,0.18,0.20,5]
+
+var num = arrint.count
+
+println(num)
+
+
+///********loopp
+
+var arr2=["a","b","c"]
+
+for (var i = 0 ; i < arr2.count;i++){
+print(arr2[i])
+
+}
+
+
+var arr3 = ["aa","bb","cc"]
+
+var str = ""
+
+for str in arr3 {
+    println(str)
+}
+
+
+let oo = ["1","2","3"]
+
+
+
+
+
+
+
+    
